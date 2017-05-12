a9740e748290d2d3dca521da861d27a8c2270fb5
robincpc <chienpinchen@gmail.com>
2017-05-12 14:11:36 -0700

    Add Dropout layer, augment data with flip images, left right camera, and plotting loss results

--- clone.py
+++ clone.py
@@ -2,6 +2,7 @@ import csv
 import cv2
 import numpy as np
 
+correction = 0.3    # correction for steer
 lines = []
 with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -10,40 +11,72 @@ with open('./data/driving_log.csv') as csvfile:
 
 images = []
 measurements = []
+cor_index = [0., 1., -1.]
 for line in lines[1:]:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = './data/IMG/' + filename
-    #print( current_path)
-    image = cv2.imread(current_path)
-    images.append(image)
-    #print( len(images))
-    measurement = float(line[3])
-    measurements.append(measurement)
+    for i in range(2):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = './data/IMG/' + filename
+        #print( current_path)
+        image = cv2.imread(current_path)
+        images.append(image)
+        #print( len(images))
+        #print(cor_index[i], correction)
+        #print(type(line[3]))
+        measurement = float(line[3]) + cor_index[i] * correction
+        measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement*-1.)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 
 # build a simple neural netowrk first
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
+model.add(Cropping2D(cropping=((50, 25), (0, 0))))
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dropout(0.5))
+model.add(Dense(100))
+model.add(Dropout(0.5))
+model.add(Dense(50))
+model.add(Dropout(0.5))
+model.add(Dense(10))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 
-# train model
+# train model and plot the loss 
+import matplotlib.pyplot as plt
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3, verbose=1)
 
 model.save('model.h5')
+
+# print the key contained in the history object
+print(history_object.history.keys())
+
+# Plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training_set', 'validation_set'], loc='upper right')
+plt.show()
+
